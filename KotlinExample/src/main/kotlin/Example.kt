5f0e5b2941acee3683977c238443d7dc67017238
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-11-27 11:09:11 +0900

    Learned about collections; List, Map and Set in ch.1

--- KotlinExample/src/main/kotlin/Example.kt
+++ KotlinExample/src/main/kotlin/Example.kt
@@ -1,14 +1,32 @@
-interface Greeter {
-    fun hello()
-}
-
-class GreeterImpl : Greeter {
-    override fun hello() {
-        println("Hello")
-    }
-}
 
 fun main() {
-    val greeter = GreeterImpl()
-    greeter.hello()
+    val intList: List<Int> = listOf<Int>(1, 2, 3)
+    println(intList)
+    println(intList[1])
+
+    val stringList: List<String> = listOf<String>("one", "two", "three")
+    println(stringList)
+    println(stringList[1])
+
+    val mutableList = mutableListOf(1, 2, 3)
+    mutableList.add(4)
+    println(mutableList)
+
+    val map: Map<Int, String> = mapOf(1 to "one", 2 to "two", 3 to "three")
+    println(map)
+    println(map[2])
+    println(map.containsKey(3))
+    println(map.containsKey(4))
+
+    val mutableMap = mutableMapOf(1 to "one", 2 to "two", 3 to "three")
+    mutableMap[4] = "four"
+    println(mutableMap)
+
+    val set = setOf("one", "two", "three")
+    println(set.contains("three"))
+    println(set.contains("four"))
+
+    val mutableSet = mutableSetOf("one", "two", "three")
+    mutableSet.add("four")
+    println(mutableSet)
 }
