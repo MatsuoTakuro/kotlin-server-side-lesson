777cab3a2147ac2013828aa3478e169837777b05
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-12-05 14:21:05 +0900

    Learned about construction injection

---
+++ demo/src/main/kotlin/com/example/demo/GreeterController.kt
@@ -0,0 +1,38 @@
+package com.example.demo
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+data class HelloRequest(val name: String)
+data class HelloResponse(val message: String)
+
+@RestController
+@RequestMapping("greeter")
+class GreeterController(
+    private val greeter: Greeter
+) {
+//class GreeterController {
+//    @Autowired
+//    private lateinit var greeter: Greeter
+
+    @GetMapping("/hello")
+    fun hello(@RequestParam("name") name: String): HelloResponse {
+        return HelloResponse("Hello $name")
+    }
+
+    @GetMapping("/hello/{name}")
+    fun helloPathValue(@PathVariable("name") name: String): HelloResponse {
+        return HelloResponse("Hello $name")
+    }
+
+    @PostMapping("/hello")
+    fun helloByPost(@RequestBody request: HelloRequest): HelloResponse {
+        return HelloResponse("Hello ${request.name}")
+    }
+
+    @GetMapping("/hello/service/{name}")
+    fun helloByService(@PathVariable("name") name: String): HelloResponse {
+        val message = greeter.sayHello(name)
+        return HelloResponse(message)
+    }
+}
